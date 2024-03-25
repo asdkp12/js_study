129e560717151a8772f2859ac4d1c0e4c875ed91
asdkp12 <dlehdgur0604@gmail.com>
2024-03-25 17:46:33 +0900

    break continue 설명 추가

--- 1. core javascript/2 .제어문 javascript/switch.js
+++ 1. core javascript/2 .제어문 javascript/switch.js
@@ -8,19 +8,19 @@ var season = prompt('계절을 입력하라우 동무')
 // 조건을 판별할 변수 (비교식x, 문자값이나 숫자값만 써야 함)
 switch (season) {
 
-  case '봄': case 'spring': 
+  case '봄': case 'spring':
     alert('봄에는 여의도로 가세요');
     break;
-  case '여름': case 'summer': 
+  case '여름': case 'summer':
     alert('여름에는 낙산해수욕장으로 가세요');
     break;
-  case '가을': case 'fall': 
+  case '가을': case 'fall':
     alert('가을에는 설악산에 가세요');
     break;
-  case '겨울': case 'winter': 
+  case '겨울': case 'winter':
     alert('겨울에는 홍천에 가세요');
     break;
   default:
     alert('계절만 입력하라우 동무');
-    
+
 }
--- 1. core javascript/4. 배열/8. quiz5.js
+++ 1. core javascript/4. 배열/8. quiz5.js
@@ -4,24 +4,26 @@ var tvxq = ['유노윤호', '최강창민', '영웅재중', '믹키유천', '시
 var menu = 0;
 var Member = '';
 while (true) {
-  menu = +prompt (`현재 멤버:${tvxq} \n메뉴를 선택하세요\n1. 새로운 멤버 추가\n2. 기존 멤버 제거\n3. 프로그램 종료`)
+  menu = +prompt(`현재 멤버:${tvxq} \n메뉴를 선택하세요\n1. 새로운 멤버 추가\n2. 기존 멤버 제거\n3. 프로그램 종료`)
   if (menu === 3) {
     break;
   } else if (menu === 1) {
-    Member = prompt (`1번 새 멤버 추가하기를 선택하셨습니다.\n새로 추가할 멤버 이름을 입력해주세요.\n '!!나가기' 를 입력하시면 초기화면으로 돌아갑니다.`);
-    if(Member === '!!나가기') continue;
+    Member = prompt(`1번 새 멤버 추가하기를 선택하셨습니다.\n새로 추가할 멤버 이름을 입력해주세요.\n '!!나가기' 를 입력하시면 초기화면으로 돌아갑니다.`);
+    if (Member === '!!나가기') continue; // break는 가장 가까운 반복문을 탈출. continue는 가장 가까운 반복문 내에서 다시 돔
     tvxq.push(Member);
   } else if (menu === 2) {
     while (true) {
-      Member = prompt (`2번 기존 멤버 제거하기를 선택하셨습니다.\n제거할 멤버의 이름을 입력해주세요.\n현재 멤버 : ${tvxq} \n '!!나가기' 를 입력하시면 초기화면으로 돌아갑니다.`);
-    if(Member === '!!나가기') break;
-    
-    if (tvxq.includes(Member)) {
-      tvxq.splice(tvxq.indexOf(Member),1); 
-    } else {
-    alert(`잘못입력하셨습니다.`);
+      Member = prompt(`2번 기존 멤버 제거하기를 선택하셨습니다.\n제거할 멤버의 이름을 입력해주세요.\n현재 멤버 : ${tvxq} \n '!!나가기' 를 입력하시면 초기화면으로 돌아갑니다.`);
+      if (Member === '!!나가기') break;
+
+      if (tvxq.includes(Member)) {
+        tvxq.splice(tvxq.indexOf(Member), 1);
+      } else {
+        alert(`잘못입력하셨습니다.`);
+      }
     }
   }
 }
-}
-alert (`멤버 : ${tvxq} \n 프로그램을 종료합니다.`)
+alert(`멤버 : ${tvxq} \n 프로그램을 종료합니다.`)
+
+// ctrl alr n 왜 안될까
