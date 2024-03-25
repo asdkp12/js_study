0c955109e67a5487d6f39c152a3096ce940d5259
asdkp12 <dlehdgur0604@gmail.com>
2024-03-25 17:12:58 +0900

    03.25

--- 1. core javascript/4. 배열/8. quiz5.js
+++ 1. core javascript/4. 배열/8. quiz5.js
@@ -8,14 +8,20 @@ while (true) {
   if (menu === 3) {
     break;
   } else if (menu === 1) {
-    Member = prompt (`1번 새 멤버 추가하기를 선택하셨습니다.\n새로 추가할 멤버 이름을 입력해주세요.`);
+    Member = prompt (`1번 새 멤버 추가하기를 선택하셨습니다.\n새로 추가할 멤버 이름을 입력해주세요.\n '!!나가기' 를 입력하시면 초기화면으로 돌아갑니다.`);
+    if(Member === '!!나가기') continue;
     tvxq.push(Member);
   } else if (menu === 2) {
-    Member = prompt (`2번 기존 멤버 제거하기를 선택하셨습니다.\n제거할 멤버의 이름을 입력해주세요.\n현재 멤버 : ${tvxq}`);
+    while (true) {
+      Member = prompt (`2번 기존 멤버 제거하기를 선택하셨습니다.\n제거할 멤버의 이름을 입력해주세요.\n현재 멤버 : ${tvxq} \n '!!나가기' 를 입력하시면 초기화면으로 돌아갑니다.`);
+    if(Member === '!!나가기') break;
+    
     if (tvxq.includes(Member)) {
       tvxq.splice(tvxq.indexOf(Member),1); 
     } else {
-    alert(`잘못입력하셨습니다 초기 메뉴로 돌아갑니다`);
+    alert(`잘못입력하셨습니다.`);
     }
   }
 }
+}
+alert (`멤버 : ${tvxq} \n 프로그램을 종료합니다.`)
