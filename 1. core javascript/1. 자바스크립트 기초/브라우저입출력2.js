1695f18722f184ff01e11820bdc2b62662bd6d59
asdkp12 <dlehdgur0604@gmail.com>
2024-03-25 10:08:00 +0900

    명시적 타입 형변환

--- 1. core javascript/1. 자바스크립트 기초/브라우저입출력2.js
+++
@@ -1,5 +0,0 @@
-var n1 = +prompt("첫번째 수를 입력하세요!");
-var n2 = +prompt("두번째 수를 입력하세요!");
-
-alert(typeof n1)
-alert(`두 수의 합 ${n1 + n2}`);
--- 1. core javascript/1. 자바스크립트 기초/입출력연습.js
+++
@@ -1,7 +0,0 @@
-var name = prompt("당신의 이름을 입력하세요");
-
-var age = +prompt("당신의 나이를 입력하세요");
-
-var year = 2024 - age + 1;
-
-alert(`${name}은(는) ${year}년생 이군요`);
